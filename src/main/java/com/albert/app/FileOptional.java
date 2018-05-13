95a9c3d411ff88c5300477560178bf80eab1c3c0
zhengzhanpeng <714586750@qq.com>
2018-05-13 18:03:47 +0800

    [Zhanpeng] add FileOptional.java

--- src/main/java/com/albert/app/FileOptional.java
+++ src/main/java/com/albert/app/FileOptional.java
@@ -1,5 +1,18 @@
 package com.albert.app;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+@Slf4j
 public class FileOptional {
 
+    public String readAllContentByFileStream(InputStream inputStream) {
+        String content;
+        try (Scanner scanner = new Scanner(inputStream)) {
+            content = scanner.useDelimiter("\\Z").next();
+        }
+        return content;
+    }
 }
--- src/test/java/com/albert/app/FileOptionalTest.java
+++ src/test/java/com/albert/app/FileOptionalTest.java
@@ -2,12 +2,16 @@ package com.albert.app;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 class FileOptionalTest {
 
     @Test
     void should_read_content_of_file_when_given_file_path() {
-
+        String content = new FileOptional().readAllContentByFileStream(
+                this.getClass().getResourceAsStream("/test_whether_can_read_the_content_in_file.md"));
+        assertNotNull(content);
+        assertEquals("hello word", content);
     }
 }
