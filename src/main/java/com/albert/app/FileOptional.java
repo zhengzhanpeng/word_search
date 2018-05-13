bc79b2d1afef3f4cda2acdb846a0d84d44746161
zhengzhanpeng <714586750@qq.com>
2018-05-13 12:56:05 +0800

    [Zhanpeng] git init

---
+++ src/main/java/com/albert/app/FileOptional.java
@@ -0,0 +1,5 @@
+package com.albert.app;
+
+public class FileOptional {
+
+}
---
+++ src/test/java/com/albert/app/FileOptionalTest.java
@@ -0,0 +1,13 @@
+package com.albert.app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileOptionalTest {
+
+    @Test
+    void should_read_content_of_file_when_given_file_path() {
+
+    }
+}
